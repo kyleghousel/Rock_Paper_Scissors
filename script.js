3ea146aa10200c7e1722c1c51ae553fc0ffcb3b5
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2022-10-31 23:54:23 -0400

    Game is fully funcitonal
    Updated to include updates to scoreboard as you play, messages to players on bottom div. Game ends at 5

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 /*
-1. Create getComputerChoice() function that returns 'rock' 'paper' or 'scissors'
+Create getComputerChoice() function that returns 'rock' 'paper' or 'scissors'
     a. Program will randomly select string from an array
 */
 const rps = ['rock', 'paper', 'scissors'];
 let playerTotal = 0;
+let playerScore = document.getElementById('humanScore');
 let computerTotal = 0; 
+let computerScore = document.getElementById('computerScore');
+let drawResult = document.getElementById('drawResult');
 
 function getComputerChoice() {
     let rando = Math.floor(Math.random() * rps.length);
@@ -13,60 +16,88 @@ function getComputerChoice() {
 
 
 /*
-2. Create playRound() function that plays round of RPS
+Create playRound() function that plays round of RPS
     a. Function should have two parameters: playerSelection computerSelection
     b. return string that declares winner
     c. make playerSelection parameter case insensitive */
 
 function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase();
-    if ((playerSelection == 'rock' & computerSelection == 'scissors') || (playerSelection === 'scissors' & computerSelection === 'paper') || (playerSelection === 'paper' & computerSelection === 'rock')){
-        playerTotal +=1;
-        return console.log(`${playerSelection} beats ${computerSelection}.`);
-    }
-    else if (playerSelection == getComputerChoice()) {
+    if ((playerSelection === 'rock' & computerSelection === 'scissors') || (playerSelection === 'scissors' & computerSelection === 'paper') || (playerSelection === 'paper' & computerSelection === 'rock')){
         
-        return console.log(`Damn, what? You both went with ${playerSelection}? Really?`);
-    }
-    else {
-        computerTotal +=1;
+        playerScore.textContent = (playerTotal +=1);
+        drawResult.textContent = '';
+        checkScore();
         
-        return console.log(`${computerSelection} beats ${playerSelection}.`);
     }
-    
+    else if (playerSelection === getComputerChoice()) {
+        
+        drawResult.textContent = `Draw! Try again.`
+        checkScore();
+    }
+    else if ((computerSelection === 'rock' & playerSelection === 'scissors') || (computerSelection === 'scissors' & playerSelection === 'paper') || (computerSelection === 'paper' & playerSelection === 'rock')) {
+        
+        computerScore.textContent = (computerTotal +=1);
+        drawResult.textContent = '';
+        checkScore();
+    }
+
 }
 
-/*
-3. Create game() function that calls playRound() inside of it
-    a. Accomodate a 5 round game **for loop**
-    b. use prompt() for user input 
-*/
 
-function game() {
+function checkScore() {
+
+    if (playerTotal == 5) {
+            drawResult.style.color = 'green';
+            drawResult.textContent = (`You won!`);
+            gameOver();
+        }
+    else if (computerTotal == 5) {
+            drawResult.textContent = (`Defeat.`);
+            gameOver();
+        }            
     
-    for (let i=0; i < 5; i++) {
-        let shoot = prompt('Rock - Paper - Scissors - SHOOT!').toLowerCase();
-        playerSelection = shoot;
-        playRound(playerSelection, getComputerChoice());
-        
-       
-      
+    
     }
-     console.log(`Player Score:${playerTotal}\nComputer Score ${computerTotal}`);
-    if (i=5 && playerTotal > computerTotal) {
-            console.log(`You win. You are an inspiration to us all.`);
-        }
-    else if (i=5 && computerTotal > playerTotal) {
-            console.log(`You lose. You suck.`);
-        }
-    else if (i=5) {
-            console.log(`No one won. It's a tie? What the hell guys?`)
-        }
-    console.log(`Player wins:${playerTotal}\nComputer wins:${computerTotal}`)
-}
-/*
-4. Score tracker function to keep score best of 5.
-    a. Keep tally of user vs computer
-    b. Once a player reaches 3 wins, they are the winner, break loop
-*/
+    
 
+
+
+
+const btnRock = document.getElementById('btn1');
+const btnPaper = document.getElementById('btn2');
+const btnScissors = document.getElementById('btn3');
+btnRock.addEventListener('click', () => {
+    clickRock();
+});
+btnPaper.addEventListener('click', () => {
+    clickPaper();
+}); 
+btnScissors.addEventListener('click', () => {
+    clickScissors();
+});  
+
+function clickRock() {
+    playRound('rock', getComputerChoice());
+    /*console.log('Player:',playerTotal);
+    console.log('Computer:',computerTotal);*/
+}
+
+function clickPaper() {
+    playRound('paper', getComputerChoice());
+    /*console.log('Player:',playerTotal);
+    console.log('Computer:',computerTotal);*/
+}
+
+function clickScissors() {
+    playRound('scissors', getComputerChoice());
+    /*console.log('Player:',playerTotal);
+    console.log('Computer:',computerTotal);*/
+}
+
+function gameOver() {
+    playerTotal = 0;
+    computerTotal = 0;
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+}
